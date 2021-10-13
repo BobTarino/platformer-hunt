5ca430278aed3c576852be435741c55e152f8ebc
Bobby T <bob.tarino@gmail.com>
2021-10-13 15:07:22 -0500

    set up index objects, functions, and event listeners

--- rabbitTrap/index.js
+++ rabbitTrap/index.js
@@ -1,41 +1,41 @@
-// render and update functions
+// wrap main javascript file in a load listener to make sure code doesn't execute until doc is finished loading and I have access to all of my classes
+window.addEventListener("load", function(event) {
 
-// called on every frame of game loop
-const render = function() {
+    "use strict";
     
-    display.renderColor(game.color);
-    display.render();
-}
+    // render and update functions
+    // called on every frame of game loop
+    const render = function() {
+        
+        display.renderColor(game.color);
+        display.render();
+    }
 
-const update = function() {
-    // tells game to change color value
-    game.update();
+    const update = function() {
+        // tells game to change color value
+        game.update();
 
-}
+    }
 
+    // game objects (defined sections) and engine
 
-
-
-
-
-
-// game sections and engine
-
-// controller handles user input
-const controller = new Controller();
-// display handles window resizing, as well as on screen canvas
-const display = new Display(document.querySelector("canvas"));
-// game variable will hold game logic
-const game = new Game();
-// engine is where the above three components can interact
-// runs at 30 frames per sec, and passes through render and update function, until we stop engine
-const engine = new Engine(1000/30, render, update);
+    // controller handles user input
+    const controller = new Controller();
+    // display handles window resizing, as well as on screen canvas
+    const display = new Display(document.querySelector("canvas"));
+    // game variable will hold game logic
+    const game = new Game();
+    // engine is where the above three components can interact
+    // runs at 30 frames per sec, and passes through render and update function, until we stop engine
+    const engine = new Engine(1000/30, render, update);
 
 
-// initialize
-window.addEventListener("resize", display.handleResize);
-window.addEventListener("keydown", controller.handleKeyDownUp);
-window.addEventListener("keyup", controller.handleKeyDownUp);
+    // initialize & instantiate 
+    window.addEventListener("resize", display.handleResize);
+    window.addEventListener("keydown", controller.handleKeyDownUp);
+    window.addEventListener("keyup", controller.handleKeyDownUp);
 
-display.resize();
-engine.start();
+    display.resize();
+    engine.start();
+
+});
