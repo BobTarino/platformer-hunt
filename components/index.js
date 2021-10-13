8aa9aea56c3d396fc197f4cfd3b3ec1592f84f74
Bobby T <bob.tarino@gmail.com>
2021-10-13 15:51:08 -0500

    set up HTML

--- components/index.js
+++ components/index.js
@@ -5,13 +5,13 @@ window.addEventListener("load", function(event) {
     
     // render and update functions
     // called on every frame of game loop
-    const render = function() {
+    var render = function() {
         
         display.renderColor(game.color);
         display.render();
     }
 
-    const update = function() {
+    var update = function() {
         // tells game to change color value
         game.update();
 
@@ -20,14 +20,14 @@ window.addEventListener("load", function(event) {
     // game objects (defined sections) and engine
 
     // controller handles user input
-    const controller = new Controller();
+    var controller = new Controller();
     // display handles window resizing, as well as on screen canvas
-    const display = new Display(document.querySelector("canvas"));
+    var display = new Display(document.querySelector("canvas"));
     // game variable will hold game logic
-    const game = new Game();
+    var game = new Game();
     // engine is where the above three components can interact
     // runs at 30 frames per sec, and passes through render and update function, until we stop engine
-    const engine = new Engine(1000/30, render, update);
+    var engine = new Engine(1000/30, render, update);
 
 
     // initialize & instantiate 
